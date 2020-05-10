a3d5d62fc5103c05ed90a9e54d9e30c12b2e578b
Max Blachman <blachmanmax@gmail.com>
2020-05-09 23:10:49 -0700

    change epoll timeout to dynamically adjust
    epoll timeout is now set to the amount of time until the next event in the
    queue should be processed. Events are now delivered closer to the exact
    ms delay with fewer wakeup sleep cycles.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
 use evdev_rs::{Device, InputEvent, UInputDevice};
+use std::collections::LinkedList;
+use std::convert::TryInto;
 use std::ffi::OsStr;
 use std::fs::{read_dir, File};
-use std::os::unix::ffi::OsStrExt;
-use std::os::unix::{fs::FileTypeExt, io::AsRawFd};
-//TODO - switch to mio
-use epoll::ControlOptions;
-use std::collections::LinkedList;
+use std::io;
+use std::os::unix::{ffi::OsStrExt, fs::FileTypeExt, io::AsRawFd};
 use std::thread::sleep;
 use std::time::{Duration, Instant};
 
-fn get_all_device_files() -> std::io::Result<Vec<File>> {
+fn get_all_device_files() -> io::Result<Vec<File>> {
     let mut res = Vec::new();
     for entry in read_dir("/dev/input")? {
         let entry = entry?;
@@ -20,32 +19,30 @@ fn get_all_device_files() -> std::io::Result<Vec<File>> {
 
         // these files don't play nice with libevdev, not sure why
         // see: https://askubuntu.com/questions/1043832/difference-between-dev-input-mouse0-and-dev-input-mice
-        entry
-            .path()
-            .file_name()
-            .map(|file_name| -> std::io::Result<()> {
-                let bytes = file_name.as_bytes();
-                // skip filenames matching "mouse.* or mice"
-                if bytes == OsStr::new("mice").as_bytes()
-                    || &bytes[0..=4] == OsStr::new("mouse").as_bytes()
-                {
-                    return Ok(());
-                }
-                println!("opening: {:?}", entry.path());
-                let file = File::open(entry.path())?;
-                res.push(file);
-                Ok(())
-            });
+        entry.path().file_name().map(|file_name| -> io::Result<()> {
+            let bytes = file_name.as_bytes();
+            // skip filenames matching "mouse.* or mice"
+            if bytes == OsStr::new("mice").as_bytes()
+                || &bytes[0..=4] == OsStr::new("mouse").as_bytes()
+            {
+                return Ok(());
+            }
+            println!("opening: {:?}", entry.path());
+            let file = File::open(entry.path())?;
+            res.push(file);
+            Ok(())
+        });
     }
     Ok(res)
 }
 
-fn main() -> std::io::Result<()> {
+fn main() -> io::Result<()> {
     let args: Vec<String> = std::env::args().collect();
     let ms_delay: u32 = args.get(1).map(|arg| arg.parse().unwrap()).unwrap_or(60);
 
+    //TODO: remove this hack
     //wait for enter key to be released after starting
-    sleep(Duration::from_millis(250));
+    sleep(Duration::from_millis(500));
 
     println!("opening /dev/input files...");
     let device_files = get_all_device_files()?;
@@ -58,7 +55,7 @@ fn main() -> std::io::Result<()> {
         let epoll_event = epoll::Event::new(epoll::Events::EPOLLIN, file_idx as u64);
         epoll::ctl(
             epoll_fd,
-            ControlOptions::EPOLL_CTL_ADD,
+            epoll::ControlOptions::EPOLL_CTL_ADD,
             device_fd,
             epoll_event,
         )?;
@@ -72,42 +69,54 @@ fn main() -> std::io::Result<()> {
             device.grab(evdev_rs::GrabMode::Grab)?;
             Ok(device)
         })
-        .collect::<std::io::Result<Vec<Device>>>()?;
+        .collect::<io::Result<Vec<Device>>>()?;
 
     let output_devices = devices
         .iter()
         .map(|device| UInputDevice::create_from_device(device))
-        .collect::<std::io::Result<Vec<UInputDevice>>>()?;
+        .collect::<io::Result<Vec<UInputDevice>>>()?;
 
-    let mut events_buffer: LinkedList<(usize, Instant, InputEvent)> = LinkedList::new();
+    let mut events_buffer = LinkedList::<(Instant, usize, InputEvent)>::new();
 
     // create buffer for epoll to fill
     let mut events = [epoll::Event::new(epoll::Events::empty(), 0); 4];
-    for _idx in 0_usize.. {
+    loop {
         //processing queued events...
         while events_buffer
             .front()
-            .map(|(_, earlier, _)| {
-                Instant::now().duration_since(*earlier) > Duration::from_millis(ms_delay.into())
-            })
+            .map(|(earlier, _, _)| earlier.elapsed() > Duration::from_millis(ms_delay.into()))
             .unwrap_or(false)
         {
-            //println!("processing events. Events remaining: {}", events_buffer.len());
-            let (idx, _, event) = events_buffer.pop_front().unwrap();
+            let (earlier, idx, event) = events_buffer.pop_front().unwrap();
             let output_device = output_devices.get(idx).unwrap();
+            let time_waited = earlier.elapsed();
+            println!(
+                "Actual amount of time waited: {}.{} ms",
+                time_waited.as_millis(),
+                time_waited.as_micros()
+            );
             output_device.write_event(&event).unwrap();
         }
-        //println!("waiting for the {}-th event...", idx);
-        let num_events = epoll::wait(epoll_fd, (ms_delay / 5) as i32, &mut events)?;
+        //wait until new events need to be processed or new events arrive
+        let wait_ms = events_buffer
+            .front()
+            .and_then(|(earlier, _, _)| {
+                Duration::from_millis(u64::from(ms_delay) + 1u64).checked_sub(earlier.elapsed())
+            })
+            .and_then(|dur| dur.as_millis().try_into().ok())
+            .unwrap_or(i32::max_value());
+        println!("waiting... {:?}ms", wait_ms);
+        //add new events to queue
+        let num_events = epoll::wait(epoll_fd, wait_ms, &mut events)?;
         for event in &events[0..num_events] {
             let device_idx = event.data as usize;
             let device = devices.get(device_idx).unwrap();
             while device.has_event_pending() {
-                let (_status, event) = device.next_event(evdev_rs::ReadFlag::NORMAL)?;
+                //TODO: deal with EV_SYN::SYN_DROPPED
+                let (_, event) = device.next_event(evdev_rs::ReadFlag::NORMAL)?;
                 //println!("Got event: {:?} status", event);
-                events_buffer.push_back((device_idx, Instant::now(), event));
+                events_buffer.push_back((Instant::now(), device_idx, event));
             }
         }
     }
-    Ok(())
 }
